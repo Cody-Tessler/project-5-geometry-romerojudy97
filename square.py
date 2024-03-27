3f64fc31ce4b99aeb509d41ebe8fb35fb20238a0
judith romero <156359081+romerojudy97@users.noreply.github.com>
2024-03-27 15:19:46 -0400

    commit

--- square.py
+++ square.py
@@ -9,9 +9,11 @@ from rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, a):
+        self.length = a
+        self.width = a
         if a < 0:
             raise ValueError("Side length must be non-negative")
-        super().__init__(a, a)  # Initialize length and width using the parent class constructor
+        # super().__init__(a, a)  # Initialize length and width using the parent class constructor
 
     def __str__(self):
         return f"Square with side length {self.length:.2f}"
